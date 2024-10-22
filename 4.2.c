830ece237563a8afd73a029b3b736aac8caf2efa
camilylima05 <silvacamily710@gmail.com>
2024-10-22 10:43:27 -0300

    Add files via upload

---
+++ 4.2.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+int main(){
+    int operacao;
+    printf("operacao:");
+    scanf("%d", &operacao);
+    if(operacao== 1){
+        printf("saldo");
+    }
+    if(operacao== 2){
+        printf("extrato");
+    }
+    if(operacao== 3){
+        printf("saque");
+    }
+    if(operacao== 4){
+        printf("sair");
+    }
+    }
+
+
